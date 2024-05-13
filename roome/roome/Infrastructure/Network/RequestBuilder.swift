200ba5f4b26c7922f8c32db9cccbd50b483e85b5
mint3382 <mskim0926@naver.com>
2024-05-13 19:16:54 +0900

    ✨(feat) Request Builder 구현

---
+++ roome/roome/Infrastructure/Network/RequestBuilder.swift
@@ -0,0 +1,45 @@
+//
+//  RequestBuilder.swift
+//  roome
+//
+//  Created by minsong kim on 5/13/24.
+//
+
+import Foundation
+
+struct RequestBuilder {
+    private let url: URL
+    private let method: HTTPMethod
+    private let bodyJSON: [String: Any]?
+    private let headers: [String: String]
+
+    init(url: URL,
+         method: HTTPMethod,
+         bodyJSON: [String: Any]?,
+         headers: [String: String] = ["Content-Type": "application/json"]) {
+
+        self.url = url
+        self.method = method
+        self.bodyJSON = bodyJSON
+        self.headers = headers
+    }
+
+    func create() -> URLRequest? {
+        var request = URLRequest(url: url)
+        
+        if let bodyJSON {
+            request.httpBody = makeRequestBody(with: bodyJSON)
+        }
+        
+        request.httpMethod = method.typeName
+        request.allHTTPHeaderFields = headers
+        
+        return request
+    }
+    
+    private func makeRequestBody(with json: [String: Any]) -> Data? {
+        let jsonData = try? JSONSerialization.data(withJSONObject: json)
+        
+        return jsonData
+    }
+}
