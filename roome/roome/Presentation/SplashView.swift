1740570a500910e27e50a9f5a0cdb4c73d9d565c
mint3382 <mskim0926@naver.com>
2024-05-20 15:01:31 +0900

    ✨(feat) SplashView 구현

---
+++ roome/roome/Presentation/SplashView.swift
@@ -0,0 +1,39 @@
+//
+//  SplashView.swift
+//  roome
+//
+//  Created by minsong kim on 5/20/24.
+//
+
+import UIKit
+
+class SplashView: UIViewController {
+    private let appDelegate = UIApplication.shared.delegate as? AppDelegate
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        setIsLogin()
+    }
+    
+    private func setIsLogin() {
+        if appDelegate?.isLogin  == true {
+            goToMain()
+        } else {
+            goToLogin()
+        }
+    }
+    
+    func goToLogin() {
+        let viewController = LoginViewController(viewModel: LoginViewModel(loginUseCase: LoginUseCase(loginRepository: LoginRepository())))
+        
+        (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?
+            .changeRootViewController(viewController, animated: true)
+    }
+    
+    func goToMain() {
+        let viewController = NicknameViewController(viewModel: NicknameViewModel(usecase: NicknameUseCase()))
+        
+        (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?
+            .changeRootViewController(viewController, animated: true)
+    }
+}
