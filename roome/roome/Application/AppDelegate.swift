c7c186f600152bbc476932b05924c0aa5656de18
mint3382 <mskim0926@naver.com>
2024-05-20 17:50:46 +0900

    ✨(feat) 애플 자동 로그인 구현

--- roome/roome/Application/AppDelegate.swift
+++ roome/roome/Application/AppDelegate.swift
@@ -7,29 +7,29 @@
 
 import UIKit
 import KakaoSDKCommon
+import AuthenticationServices
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    var isLogin: Bool = false
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         KakaoSDK.initSDK(appKey: Bundle.main.infoDictionary?["KakaoAppKey"] as! String)
         UITextField.appearance().tintColor = .roomeMain
+        appleAutomaticLogin()
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    
+    func appleAutomaticLogin() {
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        appleIDProvider.getCredentialState(forUserID: KeyChain.read(key: .userID) ?? "") { credentialState, error in
+            switch credentialState {
+            case .authorized:
+                self.isLogin = true
+            default:
+                self.isLogin = false
+            }
+        }
     }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
-
 }
 
