c3e8eb7cf58e60fce72427636648b2d8dd359087
mint3382 <mskim0926@naver.com>
2024-05-21 19:52:24 +0900

    ✨(feat) ErrorDTO 구현, NetworkError에 추가

---
+++ roome/roome/Infrastructure/Error/ErrorDTO.swift
@@ -0,0 +1,13 @@
+//
+//  ErrorDTO.swift
+//  roome
+//
+//  Created by minsong kim on 5/21/24.
+//
+
+import Foundation
+
+struct ErrorDTO: Codable {
+    let code: Int
+    let message: String
+}
--- roome/roome/Infrastructure/Error/NetworkError.swift
+++ roome/roome/Infrastructure/Error/NetworkError.swift
@@ -9,5 +9,6 @@ import Foundation
 
 enum NetworkError: Error {
     case invalidStatus(Int)
+    case failureCode(ErrorDTO)
     case noResponse
 }
