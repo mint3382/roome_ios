c2904853d9b24b35276fdb1f74cf7c2285ed58fe
mint3382 <mskim0926@naver.com>
2024-05-21 23:04:21 +0900

    ✨(feat) welcome View 구현

--- roome/roome/Presentation/View/Profile/WelcomeSignUPViewController.swift
+++ roome/roome/Presentation/View/Profile/WelcomeSignUPViewController.swift
@@ -8,23 +8,92 @@
 import UIKit
 
 class WelcomeSignUPViewController: UIViewController {
+    private let stackView: UIStackView = {
+        let stack = UIStackView()
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.axis = .vertical
+        stack.distribution = .fillProportionally
+        stack.spacing = 20
+        
+        return stack
+    }()
+    
+    private let welcomeLabel: UILabel = {
+        let label = PaddingLabel(padding: UIEdgeInsets(top: 20, left: 4, bottom: 0, right: 4))
+        label.text = """
+                    {WWW}님
+                    가입을 축하드려요!
+                    """
+        label.numberOfLines = 2
+        label.textAlignment = .center
+        label.font = UIFont().pretendardBold(size: .headline3)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+    private let descriptionLabel: UILabel = {
+        let label = PaddingLabel(padding: UIEdgeInsets(top: 0, left: 4, bottom: 20, right: 4))
+        label.text = """
+                    방탈출 취향을 가득 담은
+                    특별한 프로필을 만들어 볼까요?
+                    """
+        label.numberOfLines = 2
+        label.textAlignment = .center
+        label.font = UIFont().pretendardRegular(size: .body1)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+    private lazy var sampleImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(resource: .sample).resize(newWidth: self.view.frame.width * 0.9)
+        
+        return imageView
+    }()
+    
+    private let makeProfileButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.tintColor = .white
+        button.layer.cornerRadius = 10
+        button.backgroundColor = .roomeMain
+        button.setTitle("프로필 만들기", for: .normal)
+        button.titleLabel?.font = UIFont().pretendardBold(size: .label)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        configureStackView()
+        configureButton()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func configureStackView() {
+        view.addSubview(stackView)
+        
+        stackView.addArrangedSubview(welcomeLabel)
+        stackView.addArrangedSubview(descriptionLabel)
+        stackView.addArrangedSubview(sampleImage)
+        
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50)
+        ])
     }
-    */
+    
+    func configureButton() {
+        view.addSubview(makeProfileButton)
+        
+        NSLayoutConstraint.activate([
+            makeProfileButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -16),
+            makeProfileButton.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.05),
+            makeProfileButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.9),
+            makeProfileButton.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+    }
 
 }
