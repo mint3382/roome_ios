8fa81147867a2793ce8ef50f3d77213ef1be871a
mint3382 <mskim0926@naver.com>
2024-05-23 02:43:23 +0900

    ✨(feat) ProfileModel 구현

---
+++ roome/roome/Data/Model/ProfileModel.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileModel.swift
+//  roome
+//
+//  Created by minsong kim on 5/23/24.
+//
+
+import Foundation
+
+struct ProfileModel {
+    static let genre = ["액션", "범죄", "SF", "코미디", "로맨스", "스릴러", "공포", "판타지", "추리", "드라마", "미스터리", "감성", "어드벤처", "역사", "동화", "탈옥", "잠입", "야외", "문제방"]
+    static let strength = ["관찰력", "집중력", "분석력", "순발력", "추리력", "실행력", "창의력", "침착함", "협업 능력"]
+}
