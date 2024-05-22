c5d60451fe7e3a7d1382a56780dba82bb2880f5b
mint3382 <mskim0926@naver.com>
2024-05-22 17:21:01 +0900

    ✨(feat) 프로필 단계 표시하는 뷰 커스텀

---
+++ roome/roome/Presentation/CustomUI/ProfileStateLineView.swift
@@ -0,0 +1,69 @@
+//
+//  ProfileStateLineView.swift
+//  roome
+//
+//  Created by minsong kim on 5/22/24.
+//
+
+import UIKit
+
+class ProfileStateLineView: UIView {
+    let colorCount: CGFloat
+    let grayCount: CGFloat
+    lazy var bar = (frame.width / 12) * 0.79
+    lazy var space = (frame.width / 12) * 0.2
+    
+    init(pageNumber: CGFloat, frame: CGRect) {
+        self.colorCount = pageNumber
+        self.grayCount = 12 - pageNumber
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func draw(_ rect: CGRect) {
+        drawColorLine()
+        drawGrayLine()
+    }
+    
+    func drawColorLine() {
+        let path = UIBezierPath()
+        let end = (frame.width / CGFloat(12)) * colorCount
+        
+        let pattern: [CGFloat] = [bar, space]
+        UIColor.roomeMain.set()
+        path.move(to: CGPoint(x: 5, y: 10))
+        path.addLine(to: CGPoint(x: end, y: 10))
+        path.lineWidth = 2
+        path.lineCapStyle = .round
+        path.setLineDash(pattern, count: pattern.count, phase: 0)
+        path.stroke()
+        
+        let lineLayer = CAShapeLayer()
+        lineLayer.frame = bounds
+        lineLayer.path = path.cgPath
+        self.layer.addSublayer(lineLayer)
+    }
+    
+    func drawGrayLine() {
+        let path = UIBezierPath()
+        let start = (frame.width / CGFloat(12)) * colorCount
+        
+        let pattern: [CGFloat] = [bar, space]
+        UIColor.lightGray.set()
+        path.move(to: CGPoint(x: start + space, y: 10))
+        path.addLine(to: CGPoint(x: frame.width - 2, y: 10))
+        path.lineWidth = 2
+        path.lineCapStyle = .round
+        path.setLineDash(pattern, count: pattern.count, phase: 0)
+        path.stroke()
+        
+        let lineLayer = CAShapeLayer()
+        lineLayer.frame = bounds
+        lineLayer.path = path.cgPath
+        self.layer.addSublayer(lineLayer)
+    }
+}
+
