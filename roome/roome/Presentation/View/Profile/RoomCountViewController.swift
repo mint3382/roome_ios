fe46adda5c233bd1db4bf626a00320ac5fcd8f04
mint3382 <mskim0926@naver.com>
2024-05-22 14:19:23 +0900

    ✨(feat) RoomCount 화면 구현

---
+++ roome/roome/Presentation/View/Profile/RoomCountViewController.swift
@@ -0,0 +1,196 @@
+//
+//  RoomCountViewController.swift
+//  roome
+//
+//  Created by minsong kim on 5/22/24.
+//
+
+import UIKit
+
+class RoomCountViewController: UIViewController {
+    private let stackView: UIStackView = {
+        let stack = UIStackView()
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.axis = .vertical
+        stack.alignment = .leading
+        stack.distribution = .fillProportionally
+        stack.spacing = 4
+        
+        return stack
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = PaddingLabel(padding: UIEdgeInsets(top: 20, left: 24, bottom: 0, right: 24))
+        label.text = """
+                    현재까지 경험한 방 수를
+                    알려주세요
+                    """
+        label.numberOfLines = 2
+        label.font = UIFont().pretendardBold(size: .headline3)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+    private let descriptionLabel: UILabel = {
+        let label = PaddingLabel(padding: UIEdgeInsets(top: 0, left: 24, bottom: 20, right: 24))
+        label.text = "프로필 생성 후 마이페이지에서 수정할 수 있어요"
+        label.numberOfLines = 2
+        label.font = UIFont().pretendardRegular(size: .body1)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+    private let numberLineStackView: UIStackView = {
+        let stack = UIStackView()
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.axis = .horizontal
+        stack.distribution = .fill
+        stack.spacing = 0
+        
+        return stack
+    }()
+    
+    private let numberTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .none
+        textField.backgroundColor = .systemBackground
+        textField.textAlignment = .right
+        textField.text = "0"
+        textField.keyboardType = .numberPad
+        textField.font = UIFont().pretendardBold(size: .custom(50))
+        
+        return textField
+    }()
+    
+    private let textFieldLabel: UILabel = {
+        let label = UILabel()
+        label.text = "번"
+        label.textAlignment = .left
+        label.font = UIFont().pretendardBold(size: .custom(50))
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+    private let nextButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.tintColor = .white
+        button.layer.cornerRadius = 10
+        button.backgroundColor = .roomeMain
+        button.setTitle("다음", for: .normal)
+        button.titleLabel?.font = UIFont().pretendardBold(size: .label)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        return button
+    }()
+
+    private var nextButtonWidthConstraint: NSLayoutConstraint?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        navigationController?.isNavigationBarHidden = false
+        navigationController?.navigationBar.tintColor = .roomeMain
+        
+        numberTextField.delegate = self
+        registerKeyboardListener()
+        numberTextField.becomeFirstResponder()
+        configureUI()
+        configureNextButton()
+
+    }
+    
+    func configureUI() {
+        view.addSubview(stackView)
+        stackView.addArrangedSubview(titleLabel)
+        stackView.addArrangedSubview(descriptionLabel)
+        
+        view.addSubview(numberLineStackView)
+        numberLineStackView.addArrangedSubview(numberTextField)
+        numberLineStackView.addArrangedSubview(textFieldLabel)
+        
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            
+            numberLineStackView.topAnchor.constraint(equalTo: stackView.bottomAnchor, constant: 50),
+            numberLineStackView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor)
+        ])
+    }
+    
+    private func configureNextButton() {
+        view.addSubview(nextButton)
+        
+        nextButtonWidthConstraint = nextButton.widthAnchor.constraint(equalToConstant: view.frame.width * 0.9)
+        nextButtonWidthConstraint?.isActive = true
+        
+        NSLayoutConstraint.activate([
+            nextButton.bottomAnchor.constraint(equalTo: view.keyboardLayoutGuide.topAnchor),
+            nextButton.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.05),
+            nextButton.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+    }
+}
+
+extension RoomCountViewController: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let newText = (textField.text! as NSString).replacingCharacters(in: range, with: string)
+        
+        if newText.count == 0 {
+            nextButton.isEnabled = false
+            nextButton.backgroundColor = .gray
+        } else {
+            nextButton.isEnabled = true
+            nextButton.backgroundColor = .roomeMain
+        }
+        
+        if newText.count < 6 {
+            return true
+        } else {
+            return false
+        }
+        
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
+
+extension RoomCountViewController {
+    private func registerKeyboardListener() {
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillShow(_:)),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil
+        )
+        
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillHide(_:)),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil
+        )
+    }
+    
+    @objc private func keyboardWillShow(_ notification: Notification) {
+        nextButton.layer.cornerRadius = 0
+        
+        nextButtonWidthConstraint?.isActive = false
+        nextButtonWidthConstraint = nextButton.widthAnchor.constraint(equalToConstant: view.frame.width)
+        nextButtonWidthConstraint?.isActive = true
+    }
+    
+    @objc private func keyboardWillHide(_ notification: Notification) {
+        nextButton.layer.cornerRadius = 10
+        
+        nextButtonWidthConstraint?.isActive = false
+        nextButtonWidthConstraint = nextButton.widthAnchor.constraint(equalToConstant: view.frame.width * 0.9)
+        nextButtonWidthConstraint?.isActive = true
+    }
+}
