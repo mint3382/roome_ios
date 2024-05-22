4c7de7ca17c7ac24eb595d045ba0682e2cde5d3d
mint3382 <mskim0926@naver.com>
2024-05-23 03:42:02 +0900

    🎨(style) 폴더 정리

---
+++ roome/roome/Presentation/View/Profile/StrengthViewController.swift
@@ -0,0 +1,29 @@
+//
+//  StrengthViewController.swift
+//  roome
+//
+//  Created by minsong kim on 5/23/24.
+//
+
+import UIKit
+
+class StrengthViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
