8e1fd5419cdffa135b814e577ab01e91127f976b
mint3382 <mskim0926@naver.com>
2024-05-22 20:10:39 +0900

    ✨(feat) BackButton 구현

---
+++ roome/roome/Presentation/CustomUI/BackButton.swift
@@ -0,0 +1,28 @@
+//
+//  BackButton.swift
+//  roome
+//
+//  Created by minsong kim on 5/22/24.
+//
+
+import UIKit
+
+class BackButton: UIButton {
+    override init(frame: CGRect = .zero) {
+        super.init(frame: frame)
+        customBackButton()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func customBackButton() {
+        var buttonConfiguration = UIButton.Configuration.plain()
+        buttonConfiguration.image = UIImage(systemName: "chevron.backward")?.resize(newWidth: 20)
+        buttonConfiguration.baseForegroundColor = .label
+        
+        self.configuration = buttonConfiguration
+        self.translatesAutoresizingMaskIntoConstraints = false
+    }
+}
