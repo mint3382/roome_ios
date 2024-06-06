21d1cc85d43e114610a8f791c0b84efd4d8d06c4
mint3382 <mskim0926@naver.com>
2024-06-07 01:09:34 +0900

    ✨(feat) Dynamic Type 위해 UIFontMetrics+ 구현

--- roome/roome/Resources/Font/UIFont+.swift
+++ roome/roome/Resources/Font/UIFont+.swift
@@ -8,9 +8,9 @@
 import UIKit
 
 extension UIFont {
-    static func pretandard(name: Font.Name, size: Font.Size) -> UIFont {
-        guard let font = UIFont(name: name.file, size: size.rawValue) else {
-            return .systemFont(ofSize: size.rawValue)
+    static func pretandard(with: Font.Name, of: Font.Size) -> UIFont {
+        guard let font = UIFont(name: with.file, size: of.rawValue) else {
+            return .systemFont(ofSize: of.rawValue)
         }
         return font
     }
@@ -18,66 +18,66 @@ extension UIFont {
 
 extension UIFont {
     static var boldSpecial: UIFont {
-        return .pretandard(name: .pretendardBold, size: ._50)
+        return UIFontMetrics.customFont(with: .pretendardBold, of: ._50, for: .largeTitle)
     }
     
     static var regularHeadline1: UIFont {
-        return .pretandard(name: .pretendardRegular, size: ._32)
+        return UIFontMetrics.customFont(with: .pretendardRegular, of: ._32, for: .title1)
     }
     
     static var boldHeadline1: UIFont {
-        return .pretandard(name: .pretendardBold, size: ._32)
+        return UIFontMetrics.customFont(with: .pretendardBold, of: ._32, for: .title1)
     }
     
     static var regularHeadline2: UIFont {
-        return .pretandard(name: .pretendardRegular, size: ._28)
+        return UIFontMetrics.customFont(with: .pretendardRegular, of: ._28, for: .title2)
     }
     
     static var boldHeadline2: UIFont {
-        return .pretandard(name: .pretendardBold, size: ._28)
+        return UIFontMetrics.customFont(with: .pretendardBold, of: ._28, for: .title2)
     }
     
     static var regularHeadline3: UIFont {
-        return .pretandard(name: .pretendardRegular, size: ._24)
+        return UIFontMetrics.customFont(with: .pretendardRegular, of: ._24, for: .title3)
     }
     
     static var boldHeadline3: UIFont {
-        return .pretandard(name: .pretendardBold, size: ._24)
+        return UIFontMetrics.customFont(with: .pretendardBold, of: ._24, for: .title3)
     }
     
     static var boldTitle1: UIFont {
-        return .pretandard(name: .pretendardBold, size: ._22)
+        return UIFontMetrics.customFont(with: .pretendardBold, of: ._22, for: .subheadline)
     }
     
     static var boldTitle2: UIFont {
-        return .pretandard(name: .pretendardBold, size: ._20)
+        return UIFontMetrics.customFont(with: .pretendardBold, of: ._20, for: .body)
     }
     
     static var boldTitle3: UIFont {
-        return .pretandard(name: .pretendardBold, size: ._16)
+        return UIFontMetrics.customFont(with: .pretendardBold, of: ._16, for: .callout)
     }
     
     static var boldTitle4: UIFont {
-        return .pretandard(name: .pretendardBold, size: ._14)
+        return UIFontMetrics.customFont(with: .pretendardBold, of: ._14, for: .footnote)
     }
     
     static var regularBody1: UIFont {
-        return .pretandard(name: .pretendardRegular, size: ._16)
+        return UIFontMetrics.customFont(with: .pretendardRegular, of: ._16, for: .callout)
     }
     
     static var regularBody2: UIFont {
-        return .pretandard(name: .pretendardRegular, size: ._14)
+        return UIFontMetrics.customFont(with: .pretendardRegular, of: ._14, for: .footnote)
     }
     
     static var regularBody3: UIFont {
-        return .pretandard(name: .pretendardRegular, size: ._12)
+        return UIFontMetrics.customFont(with: .pretendardRegular, of: ._12, for: .caption1)
     }
     
     static var boldLabel: UIFont {
-        return .pretandard(name: .pretendardBold, size: ._14)
+        return UIFontMetrics.customFont(with: .pretendardBold, of: ._14, for: .footnote)
     }
     
     static var mediumCaption: UIFont {
-        return .pretandard(name: .pretendardMedium, size: ._11)
+        return UIFontMetrics.customFont(with: .pretendardMedium, of: ._11, for: .caption2)
     }
 }
