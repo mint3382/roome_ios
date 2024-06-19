355b79a2e2bed75e7c43292333461bd3591875e2
mint3382 <mskim0926@naver.com>
2024-06-19 16:33:22 +0900

    ✨(feat) MyProfile Mock DTO 추가

--- roome/roome/Common/User/Data/ProfileDTO.swift
+++ roome/roome/Common/User/Data/ProfileDTO.swift
@@ -7,6 +7,10 @@
 
 import Foundation
 
+struct MyProfileDTO {
+    static let category = ["방탈출 횟수", "MBTI", "선호 장르", "강점", "테마 중요 요소", "공포테마 포지션", "힌트 선호도", "장치/자물쇠 선호도", "활동성 선호도", "싫어하는 요소", "프로필 색상"]
+}
+
 struct ProfileDTO: Codable {
     let data: Profile
     
@@ -40,4 +44,20 @@ struct ProfileDTO: Codable {
             let endColor: String
         }
     }
+    
+    var bundle: [[String?]] {
+        [
+            [data.count],
+            [data.mbti],
+            data.preferredGenres.map { $0.title },
+            data.userStrengths.map { $0.title },
+            data.themeImportantFactors.map { $0.title },
+            [data.horrorThemePosition?.title],
+            [data.hintUsagePreference?.title],
+            [data.deviceLockPreference?.title],
+            [data.activity?.title],
+            data.themeDislikedFactors.map { $0.title }
+        ]
+    }
+        
 }
