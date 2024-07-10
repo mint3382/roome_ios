1279a8c5409f54c9f7ff29c79156644f62cbf6f0
mint3382 <mskim0926@naver.com>
2024-07-10 22:59:57 +0900

    ✨(feat) EditColorViewController 구현

---
+++ roome/roome/Main/Presentation/EditProfileCard/EditColorViewController.swift
@@ -0,0 +1,198 @@
+//
+//  EditColorViewController.swift
+//  roome
+//
+//  Created by minsong kim on 7/10/24.
+//
+
+import UIKit
+import Combine
+
+class EditColorViewController: UIViewController{
+    private let window = (UIApplication.shared.connectedScenes.first as? UIWindowScene)?.windows.first
+    private lazy var changePopUp = PopUpView(frame: window!.bounds,
+                                             title: "변경사항이 있어요",
+                                             description: "변경사항을 저장하지 않고 나가시겠어요?",
+                                             whiteButtonTitle: "취소",
+                                             colorButtonTitle: "나가기")
+    private let closeButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "xmark")?.changeImageColor(.label).resize(newWidth: 16), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        return button
+    }()
+    private let titleLabel = TitleLabel(text: "프로필 배경으로 쓰일\n색상을 골라주세요")
+    var nextButton = NextButton(title: "저장", backgroundColor: .roomeMain, tintColor: .white)
+    private lazy var flowLayout = self.createFlowLayout()
+    private lazy var collectionView = UICollectionView(frame: .zero, collectionViewLayout: self.flowLayout)
+    var viewModel: ColorSelectViewModel
+    var cancellables = Set<AnyCancellable>()
+    
+    init(viewModel: ColorSelectViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(ColorPaletteCell.self, forCellWithReuseIdentifier: "cell")
+        configureUI()
+        bind()
+    }
+    
+    func bind() {
+        nextButton.publisher(for: .touchUpInside)
+            .sink { [weak self] in
+                self?.viewModel.input.tapSaveButton.send()
+            }
+            .store(in: &cancellables)
+        
+        closeButton.publisher(for: .touchUpInside)
+            .sink { [weak self] in
+                self?.viewModel.input.tapCloseButton.send()
+            }
+            .store(in: &cancellables)
+        
+        viewModel.output.handleCloseButton
+            .throttle(for: 1, scheduler: RunLoop.main, latest: false)
+            .sink { [weak self] didEdit in
+                if let self, didEdit {
+                    window?.addSubview(changePopUp)
+                } else {
+                    self?.dismiss(animated: false)
+                }
+            }
+            .store(in: &cancellables)
+        
+        viewModel.output.handleNextButton
+            .throttle(for: 1, scheduler: RunLoop.main, latest: false)
+            .sink { [weak self] result in
+                switch result {
+                case .success(_):
+                    self?.dismiss(animated: false)
+                case .failure(let error):
+                    print(error)
+                    //TODO: error Toast 띄우기
+                }
+            }
+            .store(in: &cancellables)
+        
+        changePopUp.publisherWhiteButton()
+            .sink { [weak self] in
+                self?.changePopUp.removeFromSuperview()
+            }
+            .store(in: &cancellables)
+        
+        changePopUp.publisherColorButton()
+            .sink { [weak self] in
+                self?.changePopUp.removeFromSuperview()
+                self?.dismiss(animated: false)
+            }
+            .store(in: &cancellables)
+    }
+    
+    func configureUI() {
+        configureStackView()
+        setUpCollectionView()
+        configureNextButton()
+    }
+    
+    func configureStackView() {
+        view.addSubview(closeButton)
+        view.addSubview(titleLabel)
+        
+        NSLayoutConstraint.activate([
+            closeButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -24),
+            closeButton.widthAnchor.constraint(equalToConstant: 24),
+            closeButton.heightAnchor.constraint(equalToConstant: 24),
+            closeButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            
+            titleLabel.topAnchor.constraint(equalTo: closeButton.bottomAnchor, constant: 12),
+            titleLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor)
+        ])
+    }
+    
+    private func configureNextButton() {
+        view.addSubview(nextButton)
+        
+        NSLayoutConstraint.activate([
+            nextButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            nextButton.heightAnchor.constraint(equalToConstant: 50),
+            nextButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            nextButton.widthAnchor.constraint(equalToConstant: view.frame.width * 0.9)
+        ])
+    }
+    
+    func setUpCollectionView() {
+        collectionView.alwaysBounceVertical = true
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(collectionView)
+        
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 10),
+            collectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50)
+        ])
+    }
+
+    func createFlowLayout() -> UICollectionViewFlowLayout {
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumLineSpacing = 20
+        layout.minimumInteritemSpacing = 20
+        layout.itemSize = CGSize(width: (view.frame.width - 88) / 3 , height: (view.frame.width - 88) / 3)
+        layout.sectionInset = UIEdgeInsets(top: 10, left: 24, bottom: 50, right: 24)
+        
+        return layout
+    }
+}
+
+extension EditColorViewController: UICollectionViewDataSource, UICollectionViewDelegate  {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        UserContainer.shared.defaultProfile?.data.colors.count ?? 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? ColorPaletteCell
+        else {
+            return UICollectionViewCell()
+        }
+        guard let colorDTO = UserContainer.shared.defaultProfile?.data.colors[indexPath.row] else {
+            return UICollectionViewCell()
+        }
+        
+        let color = BackgroundColor(
+            mode: Mode(rawValue: colorDTO.mode) ?? .gradient,
+            shape: Shape(rawValue: colorDTO.shape) ?? .linear,
+            direction: Direction(rawValue: colorDTO.direction) ?? .tlBR,
+            startColor: colorDTO.startColor,
+            endColor: colorDTO.endColor)
+        
+        if let userSelect = UserContainer.shared.profile?.data.color?.id {
+            if userSelect == colorDTO.id {
+                cell.isSelected = true
+                collectionView.selectItem(at: indexPath, animated: false, scrollPosition: .init())
+                viewModel.input.selectCell.send((true, userSelect))
+            }
+        }
+        
+        cell.changeColor(color)
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        guard let colorDTO = UserContainer.shared.defaultProfile?.data.colors[indexPath.row] else {
+            return
+        }
+        viewModel.input.selectCell.send((true,colorDTO.id))
+    }
+}
--- roome/roome/ProfileSelect/Presentation/ViewModel/ColorSelectViewModel.swift
+++ roome/roome/ProfileSelect/Presentation/ViewModel/ColorSelectViewModel.swift
@@ -12,13 +12,13 @@ class ColorSelectViewModel {
     struct Input {
         let tapSaveButton = PassthroughSubject<Void, Never>()
         let selectCell = PassthroughSubject<(isEdit: Bool, id: Int), Never>()
-        let deselectCell = PassthroughSubject<Int, Never>()
+        let tapCloseButton = PassthroughSubject<Void, Never>()
     }
     
     struct Output {
         let handleCellSelect = PassthroughSubject<Result<Void, Error>,Never>()
         let handleNextButton = PassthroughSubject<Result<Void, Error>, Never>()
-        let handleCanGoNext = PassthroughSubject<Bool, Never>()
+        let handleCloseButton = PassthroughSubject<Bool, Never>()
         let handleLoading = PassthroughSubject<Bool, Never>()
     }
 
@@ -40,17 +40,9 @@ class ColorSelectViewModel {
             .sink { [weak self] (isEdit, id) in
                 if isEdit {
                     self?.id = id
-                    self?.output.handleCanGoNext.send(true)
                 } else {
                     self?.handlePage(id: id, isEdit: false)
                 }
-            }
-            .store(in: &cancellables)
-        
-        input.deselectCell
-            .sink { [weak self] id in
-                self?.id = -1
-                self?.output.handleCanGoNext.send(false)
             }
             .store(in: &cancellables)
         
@@ -61,6 +53,25 @@ class ColorSelectViewModel {
                 }
             }
             .store(in: &cancellables)
+        
+        input.tapCloseButton
+            .sink { [weak self] in
+                self?.checkEdit()
+            }
+            .store(in: &cancellables)
+    }
+    
+    private func checkEdit() {
+        let userSelect =  UserContainer.shared.defaultProfile?.data.colors.filter {
+            $0.id == id
+        }[0].id
+        let profileItem = UserContainer.shared.profile?.data.color.map { $0.id }
+        
+        if userSelect == profileItem {
+            output.handleCloseButton.send(false)
+        } else {
+            output.handleCloseButton.send(true)
+        }
     }
     
     private func handlePage(id: Int, isEdit: Bool) {
